45fb30b465391ddce4877023836ad16ab0011a47
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-05-17 17:18:26 +0530

    [3731] [Time Beats: 45.45%] [Memory Beats: 14.30%] - LeetPush

--- 100/Find-Missing-Elements.java
+++ 100/Find-Missing-Elements.java
@@ -1,15 +1,19 @@
 1class Solution {
 2    public List<Integer> findMissingElements(int[] nums) {
-3        List<Integer> l = new ArrayList<>();
+3       List<Integer> l = new ArrayList<>();
 4        Arrays.sort(nums);
 5        for (int i = 0; i < nums.length - 1; i++) {
-6            int curr = nums[i];
-7            int next = nums[i + 1];
-8            for (int j = curr + 1; j < next; j++) {
-9                l.add(j);
-10            }
-11        }
-12
-13        return l;
-14    }
-15}
+6
+7            if (nums[i] + 1 == nums[i + 1]) {
+8                continue;
+9            } else {
+10
+11                for (int j = nums[i] + 1; j < nums[i + 1]; j++) {
+12                    l.add(j);
+13                }
+14
+15            }
+16        }
+17        return l;
+18    }
+19}
