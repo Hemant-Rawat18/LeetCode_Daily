017ac1476400850ce1f4640be9944229b8c13e21
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-05-18 09:27:34 +0530

    [287] [Time Beats: 16.76%] [Memory Beats: 99.97%] - LeetPush

--- 100/Find-the-Duplicate-Number.java
+++ 100/Find-the-Duplicate-Number.java
@@ -1,11 +1,19 @@
 1class Solution {
 2    public int findDuplicate(int[] nums) {
-3        Arrays.sort(nums);
-4        for(int i=0;i<nums.length-1;i++){
-5            if(nums[i]==nums[i+1]){
-6                return nums[i];
-7            }
-8        }
-9        return -1;
-10    }
-11}
+3//        for(int i=0;i<nums.length-1;i++){
+4  //          for(int j=i+1;j<nums.length;j++){
+5    //            if(nums[i]==nums[j]){
+6//                    return nums[i];
+7  //              }
+8    //        }
+9      //  }
+10        //return -1;
+11        Arrays.sort(nums);
+12        for(int i=0;i<nums.length;i++){
+13            if(nums[i]==nums[i+1]){
+14                return nums[i];
+15            }
+16        }
+17        return -1;
+18    }
+19}
