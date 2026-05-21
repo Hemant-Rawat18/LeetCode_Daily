59509eaef9f8fe571fcdcb4f765844d19772ea54
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-05-21 12:28:20 +0530

    [88] [Time Beats: 13.62%] [Memory Beats: 7.01%] - LeetPush

--- 100/Merge-Sorted-Array.java
+++ 100/Merge-Sorted-Array.java
@@ -1,17 +1,19 @@
 1class Solution {
 2    public void merge(int[] nums1, int m, int[] nums2, int n) {
-3        ArrayList<Integer>nums=new ArrayList<>();
-4        for(int i=0;i<m;i++){
-5            nums.add(nums1[i]);
-6        }
-7        for(int i=0;i<n;i++){
-8            nums.add(nums2[i]);
-9        }
-10        Collections.sort(nums);
-11        for(int i=0;i<nums1.length;i++){
-12            nums1[i]=nums.get(i);
+3        // for(int i=m;i<m+n;i++){
+4        //     nums1[i]=nums2[i-m];
+5        // }
+6        // Arrays.sort(nums1);
+7        int arr[]=new int[m+n];
+8        for(int i=0;i<m;i++){
+9            arr[i]=nums1[i];
+10        }
+11         for(int i=m;i<m+n;i++){
+12            arr[i]=nums2[i-m];
 13        }
-14
-15        
-16    }
-17}
+14        Arrays.sort(arr);
+15      for(int i=0;i<m+n;i++){
+16        nums1[i]=arr[i];
+17      }
+18    }
+19}
